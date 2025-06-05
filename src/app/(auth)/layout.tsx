286224d9f12b8205c48ee9f067c0dbf189bfe6fe
igee-17 <igbagboleye@gmail.com>
2025-06-05 22:05:55 +0100

    removed social links from auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,9 @@
 "use client";
-import Footer from "@/components/Footer";
-import LandingNavbar from "@/components/Landing/LandingNavbar";
-import SocialLinks from "@/components/SocialLinks";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      {children}
-      <SocialLinks />
-    </>
-  );
+  return <>{children}</>;
 }
